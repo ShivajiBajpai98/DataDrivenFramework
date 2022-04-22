0bffe4396faa066f5948357f9b876c4309986199
ShivajiBajpai98 <84443265+ShivajiBajpai98@users.noreply.github.com>
2022-04-22 10:29:33 +0530

    commit

--- src/test/java/atmecs/utilities/ExtentManager.java
+++ src/test/java/atmecs/utilities/ExtentManager.java
@@ -22,9 +22,9 @@ public class ExtentManager {
         
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
-        extent.setSystemInfo("Automation Tester", "Rahul Arora");
-        extent.setSystemInfo("Organization", "Way2Automation");
-        extent.setSystemInfo("Build no", "W2A-1234");
+      //  extent.setSystemInfo("Automation Tester", "Shivaji Bajpai");
+       // extent.setSystemInfo("Organization", " ");
+        //extent.setSystemInfo("Build no", "");
         
         
         return extent;
